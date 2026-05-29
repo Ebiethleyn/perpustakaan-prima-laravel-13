5ec5afcd05c65d8981097b5142bfe79b688de6e5
Gabriel Daten Leyn <0232gabbrieldaten@gmail.com>
2026-05-29 22:20:33 +0700

    feat: membuat migration dan model Buku sesuai dengan schema database

---
+++ app/Models/Buku.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+#[Fillable(['judul', 'penulis', 'penerbit', 'tahun_terbit'])]
+class Buku extends Model
+{
+    use HasFactory;
+
+    // 1. Menegaskan nama tabel wajib tunggal 'buku'
+    protected $table = 'buku';
+
+    // 2. Menegaskan primary key wajib 'bukuId'
+    protected $primaryKey = 'bukuId';
+}
