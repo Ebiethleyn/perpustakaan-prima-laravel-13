3c7117b98d0bd04da80f376286dd198edebb6924
Gabriel Daten Leyn <0232gabbrieldaten@gmail.com>
2026-05-29 22:41:45 +0700

    feat: membuat migration dan model KoleksiPribadi untuk fitur penanda buku favorit

---
+++ database/migrations/2026_05_29_154040_create_koleksi_pribadis_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('koleksipribadi', function (Blueprint $table) {
+            $table->integer('KoleksiId')->autoIncrement();
+            $table->integer('UserId')->nullable();
+            $table->integer('BukuId')->nullable();
+            $table->timestamps();
+
+            // Relasi Foreign Key sesuai dengan file SQL asli
+            $table->foreign('UserId')
+                ->references('UserId')->on('user')
+                ->onDelete('cascade');
+
+            $table->foreign('BukuId')
+                ->references('bukuId')->on('buku')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('koleksipribadi');
+    }
+};
