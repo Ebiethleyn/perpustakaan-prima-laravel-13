2085daba4dd910bd93bddf29323b866f491c5232
Gabriel Daten Leyn <0232gabbrieldaten@gmail.com>
2026-05-29 22:10:47 +0700

    feat: menyesuaikan struktur tabel migration user sesuai dengan schema database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,29 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('user', function (Blueprint $table) {
+            $table->integer('UserId')->autoIncrement();
+            $table->string('username', 50);
+            $table->string('password', 255);
+            $table->string('email', 100);
+            $table->string('namaLengkap', 100);
+            $table->text('alamat')->nullable();
+            $table->enum('role', ['administrator', 'petugas', 'peminjam']);
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
